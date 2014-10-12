7f356c82dd796071e2eb153a9e338d9336db6b12
Stelian Nastase <stelian.nastase@gmail.com>
2014-10-12 16:06:35 +0200

    Clean phone features

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,14 +6,14 @@ var photosControllers = angular.module('photosControllers', []);
 
 photosControllers.controller('PhotoListCtrl', ['$scope', 'Photo',
   function($scope, Photo) {
-    $scope.phones = Photo.query();
+    $scope.photos = Photo.query();
     $scope.orderProp = 'age';
   }]);
 
 photosControllers.controller('PhotoDetailCtrl', ['$scope', '$routeParams', 'Photo',
   function($scope, $routeParams, Photo) {
     $scope.photo = Photo.get({photoId: $routeParams.photoId}, function(photo) {
-      $scope.mainImageUrl = photo.images[0];
+      $scope.mainImageUrl = photo.imageUrl;
     });
 
     $scope.setImage = function(imageUrl) {
